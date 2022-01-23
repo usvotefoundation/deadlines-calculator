a43a23a9e98980e4de8e25c5fad051ac25b26481
Jalia <evans.jalia@gmail.com>
2022-01-23 15:23:29 -0800

    formatting login page, adding mui, moment and dateFormat to json

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import Button from '@mui/material/Button';
 
 import awsExports from './aws-exports';
 import React from 'react';
+import './styles/main.scss';
+
 Amplify.configure(awsExports);
 
-
 function App({ signOut, user }) {
-  console.log(user)
-
   return (
-    <div>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-      </div>
+    <div className="container main-content">
+      <header className="app-header">
+        <div className="title-container">
+          <h1>Voting Deadline Calculator</h1>
+        </div>
+        <div className="sign-out">
+          <div className="username-container">
+              {user.attributes.preferred_username}
+            </div>
+          <Button variant="outlined" color="info" onClick={signOut}>Sign Out</Button>
+        </div>
+      </header>
+      {/* Search Form, where you can see the contents of the calculator and the deadlines */}
+      {/* <SearchForm /> */}
+
+      {/* Add and delete data to the database en masse */}
+      {/* <AddDelete />  */}
+  </div>
   );
 }
 
