bcea82b038479757d2ace9607c5d387b485632c1
Jalia <evans.jalia@gmail.com>
2022-02-08 10:13:29 -0800

    emptying cli.json

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -13,7 +13,6 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "api": {
         "newcalculator": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
